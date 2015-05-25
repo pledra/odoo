077d43a27506dbcc406a73bf96a347b2e84c9ec1
qdp-odoo <qdp@odoo.com>
2015-05-25 21:56:05 +0200

    [IMP] account: usability improvements

--- addons/account_batch_deposit/batch_deposit.py
+++ addons/account_batch_deposit/batch_deposit.py
@@ -11,7 +11,7 @@ class AccountBatchDeposit(models.Model):
     name = fields.Char(required=True, states={'sent': [('readonly', True)]}, copy=False, string='Reference')
     date = fields.Date(required=True, states={'sent': [('readonly', True)]}, copy=False, default=fields.Date.context_today)
     state = fields.Selection([('draft', 'New'), ('sent', 'Printed'), ('reconciled', 'Reconciled')], readonly=True, default='draft', copy=False)
-    journal_id = fields.Many2one('account.journal', string='Journal', domain=[('type', '=', 'bank')], required=True, states={'sent': [('readonly', True)]})
+    journal_id = fields.Many2one('account.journal', string='Bank', domain=[('type', '=', 'bank')], required=True, states={'sent': [('readonly', True)]})
     payment_ids = fields.One2many('account.payment', 'batch_deposit_id', required=True, states={'sent': [('readonly', True)]})
     amount = fields.Monetary(compute='_compute_amount', store=True, readonly=True)
     currency_id = fields.Many2one('res.currency', compute='_compute_currency', store=True, readonly=True)
