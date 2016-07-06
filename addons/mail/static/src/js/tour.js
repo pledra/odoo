4f46e72badf65dd9904d7e3a4a6aa0669573d6f4
qsm-odoo <qsm@odoo.com>
2016-07-06 13:47:31 +0200

    [ADD] mail: auto tour

--- addons/mail/static/src/js/tour.js
+++ addons/mail/static/src/js/tour.js
@@ -6,15 +6,28 @@ var tour = require('web_tour.tour');
 
 var _t = core._t;
 
-tour.register('mail_tour', [{
+tour.register('mail_tour', {
+    url: "/web#action=mail.mail_channel_action_client_chat",
+}, [{
     trigger: '.o_mail_chat .o_mail_chat_sidebar .o_add[data-type="public"]',
     content: _t("<p>Channels make it easy to organize information across different topics and groups.</p> <p>Try to <b>create your first channel</b> (e.g. sales, marketing, product XYZ, after work party, etc).</p>"),
     position: 'bottom',
+    run: function (actions) {
+        actions.auto();
+        actions.auto(".o_mail_add_channel > input");
+    },
+}, {
+    trigger: ".ui-menu-item",
+    auto: true,
 }, {
     trigger: '.o_mail_chat .o_composer_text_field',
     content: _t("<p><b>Write a message</b> to the members of the channel here.</p> <p>You can notify someone with <i>\'@\'</i> or link another channel with <i>\'#\'</i>. Start your message with <i>\'/\'</i> to get the list of possible commands.</p>"),
     position: "top",
     width: 350,
+    run: function (actions) {
+        actions.auto();
+        actions.auto(".o_composer_button_send");
+    },
 }, {
     trigger: ".o_mail_chat .o_mail_thread .o_thread_message_star",
     content: _t("Messages can be <b>starred</b> to remind you to check back later."),
