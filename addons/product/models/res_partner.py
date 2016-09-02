e8f2739743a1b1198c850299411bfcfe72b0e1a6
Jeremy Kersten <jke@odoo.com>
2016-09-02 22:06:10 +0200

    MERGEM_ME_I_PROPERTY

--- addons/product/models/res_partner.py
+++ addons/product/models/res_partner.py
@@ -10,14 +10,15 @@ class Partner(models.Model):
 
     property_product_pricelist = fields.Many2one(
         'product.pricelist', 'Sale Pricelist', compute='_compute_product_pricelist',
-        inverse="_inverse_product_pricelist", company_dependent=False,  # needed to disable the property
+        inverse="_inverse_product_pricelist", company_dependent=False,  # NOT A REAL PROPERTY
         help="This pricelist will be used, instead of the default one, for sales to the current partner")
 
     @api.multi
     @api.depends('country_id')
     def _compute_product_pricelist(self):
         for p in self:
-            p.property_product_pricelist = self.env['product.pricelist'].get_partner_pricelist(p.id)
+            if not isinstance(p.id, models.NewId):  # if not onchange
+                p.property_product_pricelist = self.env['product.pricelist']._get_partner_pricelist(p.id)
 
     @api.one
     def _inverse_product_pricelist(self):
