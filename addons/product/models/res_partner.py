148ab401020963fbf51cc04dfa9e9325d0a339b4
Jeremy Kersten <jke@odoo.com>
2016-09-02 12:54:06 +0200

    [IMP] product: make property_product_pricelit dynamic for res partner '

--- addons/product/models/res_partner.py
+++ addons/product/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class Partner(models.Model):
@@ -9,8 +9,18 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     property_product_pricelist = fields.Many2one(
-        'product.pricelist', 'Sale Pricelist', company_dependent=True,
+        'product.pricelist', 'Sale Pricelist', compute='_compute_product_pricelist',
+        inverse="_inverse_product_pricelist", company_dependent=False,  # needed to disable the property
         help="This pricelist will be used, instead of the default one, for sales to the current partner")
+
+    @api.multi
+    def _compute_product_pricelist(self):
+        for p in self:
+            p.property_product_pricelist = self.env['product.pricelist'].get_partner_pricelist(p.id)
+
+    @api.one
+    def _inverse_product_pricelist(self):
+        self.env['ir.property'].set_multi('property_product_pricelist', self._name, {self.id: self.property_product_pricelist})
 
     def _commercial_fields(self):
         return super(Partner, self)._commercial_fields() + ['property_product_pricelist']
