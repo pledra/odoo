b5d2761e6d3e817e3a688df1ec3dc6a3b60e9d5c
Jeremy Kersten <jke@odoo.com>
2016-09-02 09:41:10 +0200

    remove_property_temp

--- addons/product/models/res_partner.py
+++ addons/product/models/res_partner.py
@@ -8,9 +8,13 @@ class Partner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
+    # property_product_pricelist = fields.Many2one(
+    #     'product.pricelist', 'Sale Pricelist', compute='_compute_product_pricelist',
+    #     inverse="_inverse_product_pricelist", company_dependent=False,  # needed to disable the property
+    #     help="This pricelist will be used, instead of the default one, for sales to the current partner")
+
     property_product_pricelist = fields.Many2one(
-        'product.pricelist', 'Sale Pricelist', compute='_compute_product_pricelist',
-        inverse="_inverse_product_pricelist", company_dependent=False,  # needed to disable the property
+        'product.pricelist', 'Sale Pricelist', company_dependent=True,
         help="This pricelist will be used, instead of the default one, for sales to the current partner")
 
     @api.multi
