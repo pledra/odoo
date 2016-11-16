ab3f8954f1bd0f233c7e172ac5ca87dc6286b307
Denis Vermylen (dve) <dve@odoo.com>
2016-11-16 10:19:57 +0100

    [IMP] product: add displayed weight and volume setting
    You can now configure the displayed UoM for weight and volume in Settings ->
    Configuration, this will be a company-dependent setting.
    You need to already manage multiple UoMs (group_uom, sale/stock setting) to
    have access to the setting.
    
    Weight and Volume widgets will convert it to the chosen UoM.

--- addons/product/models/base_config_settings.py
+++ addons/product/models/base_config_settings.py
@@ -12,6 +12,8 @@ class BaseConfigSettings(models.TransientModel):
         help="Share your product to all companies defined in your instance.\n"
              " * Checked : Product are visible for every company, even if a company is defined on the partner.\n"
              " * Unchecked : Each company can see only its product (product where company is defined). Product not related to a company are visible for all companies.")
+    weight_uom_id = fields.Many2one(related='company_id.weight_uom_id')
+    volume_uom_id = fields.Many2one(related='company_id.volume_uom_id')
 
     @api.model
     def get_default_company_share_product(self, fields):
--- addons/product/models/res_company.py
+++ addons/product/models/res_company.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, _
+from odoo import api, fields, models, _
 
 
 class ResCompany(models.Model):
     _inherit = "res.company"
+
+    def _default_weight_uom_id(self):
+        kg_uom = self.env.ref('product.product_uom_kgm', raise_if_not_found=False)
+        # necessary check as data not yet loaded on module installation
+        if kg_uom:
+            return kg_uom.id
+
+    def _default_volume_uom_id(self):
+        litre_uom = self.env.ref('product.product_uom_litre', raise_if_not_found=False)
+        # necessary check as data not yet loaded on module installation
+        if litre_uom:
+            return litre_uom.id
+
+    weight_uom_id = fields.Many2one(
+        'product.uom', 'Weight unit of measure',
+        default=_default_weight_uom_id,
+        domain=lambda self: [('category_id', '=', self.env.ref('product.product_uom_categ_kgm').id)],
+        help="This company will display weights in this unit of measure.")
+    volume_uom_id = fields.Many2one(
+        'product.uom', 'Volume unit of measure',
+        default=_default_volume_uom_id,
+        domain=lambda self: [('category_id', '=', self.env.ref('product.product_uom_categ_vol').id)],
+        help="This company will display volumes in this unit of measure.")
 
     @api.model
     def create(self, vals):
@@ -38,7 +61,7 @@ class ResCompany(models.Model):
             nb_companies = self.search_count([])
             for company in self:
                 existing_pricelist = ProductPricelist.search(
-                    [('company_id', 'in', (False, company.id)), 
+                    [('company_id', 'in', (False, company.id)),
                      ('currency_id', '=', currency_id)])
                 if existing_pricelist:
                     continue
@@ -55,7 +78,7 @@ class ResCompany(models.Model):
                         'company': company.name
                     }
                     pricelist = ProductPricelist.create({
-                        'name': _("Default %(currency)s pricelist for %(company)s") %  params,
+                        'name': _("Default %(currency)s pricelist for %(company)s") % params,
                         'currency_id': currency_id,
                         'company_id': company.id,
                     })
