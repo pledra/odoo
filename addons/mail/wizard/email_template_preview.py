a20eaf14704cf661b7b3f9be2e8cd4af7a91b7fa
Damien Bouvy <dbo@odoo.com>
2016-12-14 00:23:29 +0100

    [IMP] mail: allow selecting any record in mail preview
    Right now, mail preview wizards somehow fetch 10 records and allow
    them to preview your mail template on them.
    
    This is quite limited, as you would ideally like to be able to choose
    a particular record (if certain values are set, etc.).
    
    This commit make use of the new reference_m2o widget to allow this
    behaviour transparently using a reference field.

--- addons/mail/wizard/email_template_preview.py
+++ addons/mail/wizard/email_template_preview.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class TemplatePreview(models.TransientModel):
@@ -10,38 +11,30 @@ class TemplatePreview(models.TransientModel):
     _description = "Email Template Preview"
 
     @api.model
-    def _get_records(self):
-        """ Return Records of particular Email Template's Model """
-        template_id = self._context.get('template_id')
-        default_res_id = self._context.get('default_res_id')
-        if not template_id:
-            return []
-        template = self.env['mail.template'].browse(int(template_id))
-        records = self.env[template.model_id.model].search([], limit=10)
-        records |= records.browse(default_res_id)
-        return records.name_get()
+    def _get_reference_model(self):
+        template = self.env['mail.template'].browse(self._context.get('active_id'))
+        return [(template.model_id.model, template.model_id.name)]
 
     @api.model
-    def default_get(self, fields):
-        result = super(TemplatePreview, self).default_get(fields)
+    def _get_default_preview(self):
+        template = self.env['mail.template'].browse(self._context.get('active_id'))
+        model = template.model_id.model
+        record = self.env[model].search([], limit=1)
+        if not record:
+            raise UserError('There is no record on which to preview this template.')
+        return "%s, %s" % (model, record.id)
 
-        if 'res_id' in fields and not result.get('res_id'):
-            records = self._get_records()
-            result['res_id'] = records and records[0][0] or False  # select first record as a Default
-        if self._context.get('template_id') and 'model_id' in fields and not result.get('model_id'):
-            result['model_id'] = self.env['mail.template'].browse(self._context['template_id']).model_id.id
-        return result
-
-    res_id = fields.Selection(_get_records, 'Sample Document')
     partner_ids = fields.Many2many('res.partner', string='Recipients')
+    preview_record = fields.Reference(string="Preview on", selection=_get_reference_model,
+                                      default=_get_default_preview)
 
-    @api.onchange('res_id')
+    @api.onchange('preview_record')
     @api.multi
     def on_change_res_id(self):
         mail_values = {}
-        if self.res_id and self._context.get('template_id'):
+        if self.preview_record and self._context.get('template_id'):
             template = self.env['mail.template'].browse(self._context['template_id'])
             self.name = template.name
-            mail_values = template.generate_email(self.res_id)
+            mail_values = template.generate_email(self.preview_record.id)
         for field in ['email_from', 'email_to', 'email_cc', 'reply_to', 'subject', 'body_html', 'partner_to', 'partner_ids', 'attachment_ids']:
             setattr(self, field, mail_values.get(field, False))
