1003ee5dd161c93bee4b43598052473629ef32d4
Lucas Perais (lpe) <lpe@odoo.com>
2016-12-19 15:34:43 +0100

    Owner condition redone

--- addons/hw_screen/controllers/main.py
+++ addons/hw_screen/controllers/main.py
@@ -129,6 +129,7 @@ class HardwareScreen(openerp.addons.web.controllers.main.Home):
     @http.route('/point_of_sale/test_ownership', type='json', auth='none', cors='*')
     def test_ownership(self):
         global pos_client_data
-        if not pos_client_data.get('ip_from') == http.request.httprequest.remote_addr:
+        if pos_client_data.get('ip_from') == http.request.httprequest.remote_addr:
+            return {'status': 'OWNER'}
+        else:
             return {'status': 'NOWNER'}
-        return {'status': 'OWNER'}
