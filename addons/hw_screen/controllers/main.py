f70b3e3de0848296f0301477f0fcdc77752fff95
Lucas Perais (lpe) <lpe@odoo.com>
2016-12-22 16:29:27 +0100

    Added Refresh Firefox

--- addons/hw_screen/controllers/main.py
+++ addons/hw_screen/controllers/main.py
@@ -53,7 +53,7 @@ class HardwareScreen(openerp.addons.web.controllers.main.Home):
         os.environ['DISPLAY'] = ":0"
         os.environ['XAUTHORITY'] = "/tmp/.Xauthority"
         call(['sudo', 'xdotool', 'key', 'F5'])
-        return True
+        return "Display Refreshed"
 
 
     # POS CASHIER'S ROUTES
