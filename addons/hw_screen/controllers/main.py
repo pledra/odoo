beb485181ac15c0a1700ea0670f7667ddc1db328
Lucas Perais (lpe) <lpe@odoo.com>
2017-01-16 09:17:09 +0100

    Make use of /web/static for bootstrap and jquery

--- addons/hw_screen/controllers/main.py
+++ addons/hw_screen/controllers/main.py
@@ -28,6 +28,9 @@ import os
 import threading
 import netifaces as ni
 from subprocess import call
+import openerp.tools.config as config
+
+self_port = str(config['xmlrpc_port'] or 8069)
 
 _logger = logging.getLogger(__name__)
 
@@ -108,18 +111,10 @@ class HardwareScreen(openerp.addons.web.controllers.main.Home):
 
     def _get_html(self):
         cust_js = None
-        jquery = None
-        bootstrap = None
         interfaces = ni.interfaces()
 
         with open(os.path.join(os.path.dirname(__file__), "../static/src/js/worker.js")) as js:
             cust_js = js.read()
-
-        with open(os.path.join(os.path.dirname(__file__), "../static/src/lib/jquery-3.1.1.min.js")) as jq:
-            jquery = jq.read()
-
-        with open(os.path.join(os.path.dirname(__file__), "../static/src/lib/bootstrap.css")) as btst:
-            bootstrap = btst.read()
 
         with open(os.path.join(os.path.dirname(__file__), "../static/src/css/cust_css.css")) as css:
             cust_css = css.read()
@@ -139,15 +134,13 @@ class HardwareScreen(openerp.addons.web.controllers.main.Home):
             <html>
                 <head>
                 <title class="origin">Odoo -- Point of Sale</title>
-                <script type="text/javascript" class="origin">
-                    """ + jquery + """
+                <script type="text/javascript" class="origin" src="http://127.0.0.1:""" + self_port + """/web/static/lib/jquery/jquery.js" >
                 </script>
                 <script type="text/javascript" class="origin">
                     """ + cust_js + """
                 </script>
-                <style class="origin">
-                    """ + bootstrap + """
-                </style>
+                <link rel="stylesheet" class="origin" href="http://127.0.0.1:""" + self_port + """/web/static/lib/bootstrap/css/bootstrap.css" >
+                </link>
                 <style class="origin">
                     """ + cust_css + """
                 </style>
@@ -175,4 +168,3 @@ class HardwareScreen(openerp.addons.web.controllers.main.Home):
                 </html>
             """
         return html
-
