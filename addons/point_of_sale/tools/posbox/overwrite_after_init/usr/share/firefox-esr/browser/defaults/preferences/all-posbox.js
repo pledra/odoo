0d24def48ad79e160b17c7a565b988584ac2b39c
Lucas Perais (lpe) <lpe@odoo.com>
2017-02-01 11:05:30 +0100

    [IMP][POSBOX] point_of_sale: the resources needed to build the image of the posbox have been modified to have an out of the box firefox in kiosk mode, within a lightdm as a service
    [DEL] point_of_sale: no need for resources of the previous implementation of hw_screen

---
+++ addons/point_of_sale/tools/posbox/overwrite_after_init/usr/share/firefox-esr/browser/defaults/preferences/all-posbox.js
@@ -0,0 +1,6 @@
+// ODOO R-KIOSK PREFERENCES
+pref("app.update.checkInstallTime", false);
+pref("devtools.webide.widget.autoinstall", false);
+pref("xpinstall.customConfirmationUI", false);
+pref("xpinstall.signatures.required", false);
+pref("browser.shell.checkDefaultBrowser",false)
