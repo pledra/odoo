309fd663391c7bbec50dc897790bc10baae62eb7
Lucas Perais (lpe) <lpe@odoo.com>
2017-03-15 13:58:14 +0100

    [FIX] hw_screen: correcting worker.js after ged review

--- addons/hw_screen/static/src/js/worker.js
+++ addons/hw_screen/static/src/js/worker.js
@@ -1,4 +1,7 @@
     $(function() {
+        "use strict";
+        // mergedHead will be turned to true the first time we receive something from a new host
+        // It allows to transform the <head> only once
         var mergedHead = false;
         var current_client_url = "";
 
@@ -12,8 +15,8 @@
 
                 success: function(data) {
                     var trimmed = $.trim(data.result.rendered_html);
-                    var parsedHTML = $('.shadow').html($.parseHTML(trimmed,keepScripts=true));
-                    var new_client_url = $(".resources > base",parsedHTML).attr('href');
+                    var $parsedHTML = $('<div>').html($.parseHTML(trimmed,true)); // WARNING: the true here will executes any script present in the string to parse
+                    var new_client_url = $parsedHTML.find(".resources > base").attr('href');
 
                     if (!mergedHead || (current_client_url !== new_client_url)) {
 
@@ -21,18 +24,20 @@
                         current_client_url = new_client_url;
                         $("body").removeClass('original_body').addClass('ajax_got_body');
                         $("head").children().not('.origin').remove();
-                        $("head").append($(".resources",parsedHTML).html());
+                        $("head").append($parsedHTML.find(".resources").html());
                     } 
 
-                    $(".resources",parsedHTML).remove();
-                    $(".container").html($('.pos-customer_facing_display', parsedHTML).html());
-                    $(".container").attr('class', 'container').addClass($('.pos-customer_facing_display', parsedHTML).attr('class'));
-                    parsedHTML.filter('script').each(function() {
-                            $.globalEval(this.text || this.textContent || this.innerHTML || '');});   
-                    $(".shadow").html(""); 
+                    $(".container").html($parsedHTML.find('.pos-customer_facing_display').html());
+                    $(".container").attr('class', 'container').addClass($parsedHTML.find('.pos-customer_facing_display').attr('class'));
+
                     var d = $('.pos_orderlines_list');
                     d.scrollTop(d.prop("scrollHeight"));
-             
+                    
+                    // Here we execute the code coming from the pos, apparently $.parseHTML() executes scripts right away,
+                    // Since we modify the dom afterwards, the script might not have any effect
+                    if (typeof foreign_js !== 'undefined' && $.isFunction(foreign_js)) {
+                        foreign_js();
+                    }
                 },
 
                 complete: function(jqXHR,err) {
