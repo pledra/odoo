aebaa1d8362789b46643da68d0a3bd8784a1a34f
Deep Patel <dep@odoo.com>
2017-05-18 18:34:57 +0530

    [IMP] POS : add keyboard events on pos product selection interface

--- addons/point_of_sale/static/src/js/screens.js
+++ addons/point_of_sale/static/src/js/screens.js
@@ -963,6 +963,35 @@ var ProductScreenWidget = ScreenWidget.extend({
         if (_.size(this.action_buttons)) {
             this.$('.control-buttons').removeClass('oe_hidden');
         }
+        this._onKeyDown = function (event) {
+            //prevent input and textarea keydown event
+            if(!_.contains(["INPUT", "TEXTAREA"], $(event.target).prop('tagName'))){
+                switch(event.keyCode) {
+                    case $.ui.keyCode.BACKSPACE:
+                        self.numpad.clickDeleteLastChar();
+                        break;
+                    case 110:
+                        self.numpad.state.appendNewChar('.');
+                        break;
+                    case 107:
+                    case 109:
+                        self.numpad.clickSwitchSign();
+                        break;
+                    case $.ui.keyCode.DELETE:
+                        self.numpad.state.resetValue();
+                        self.numpad.state.trigger('set_value',0);
+                        break;
+                }
+            }
+        };
+        //we have to use keypress event along with keydown because in keypress numkey(numeric keys,numeric keypad) have same key code
+        this._onKeyPress = function(event) {
+            if(!_.contains(["INPUT", "TEXTAREA"], $(event.target).prop('tagName'))){
+                if (event.keyCode >= 48 && event.keyCode <= 57) { // Numkey 0-9
+                    self.numpad.state.appendNewChar((event.keyCode - 48).toString());
+                }
+            }
+        };
     },
 
     click_product: function(product) {
@@ -979,6 +1008,14 @@ var ProductScreenWidget = ScreenWidget.extend({
             this.product_categories_widget.reset_category();
             this.numpad.state.reset();
         }
+        $(document).on('keydown.productscreen',this._onKeyDown);
+        $(document).on('keypress.productscreen',this._onKeyPress);
+    },
+
+    hide: function() {
+        this._super();
+        $(document).off('keydown.productscreen',this._onKeyDown);
+        $(document).off('keypress.productscreen',this._onKeyPress);
     },
 
     close: function(){
