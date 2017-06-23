bf6feb70d55c667a0ddd37961061a9b2d2175e65
Sanjay Jamod <sja@odoo.com>
2017-06-23 18:53:50 +0530

    [IMP] puchase: Improved testcase for,
    Case 1 - When vendor pricelist applied
    =======================================
        When changing product quantity, if quantity is less than vendor's pricelist's minimum quantity then the price unit should set to 0
    
    Case 2 - When vendor pricelist applied
    =======================================
        when changing product UoM, if product's UoM is not equal to vendor's pricelist's Uom then the price unit should set to 0
    
    Case 2 - When vendor pricelist not applied
    =======================================
        This is for regression testing
        select a product which does not have vendor pricelist
        set price unit 100 and prduct qty 5 and check subtotal is 500(call to onchange_product should not disturb manually changed
    unit price in this case)

--- addons/purchase/tests/test_onchange_product_id.py
+++ addons/purchase/tests/test_onchange_product_id.py
@@ -22,12 +22,15 @@ class TestOnchangeProductId(TransactionCase):
         self.product_model = self.env['product.product']
         self.product_uom_model = self.env['product.uom']
         self.supplierinfo_model = self.env["product.supplierinfo"]
+        self.product_id_2 = self.env.ref('product.product_product_4')
 
     def test_onchange_product_id(self):
 
         uom_id = self.product_uom_model.search([('name', '=', 'Unit(s)')])[0]
+        uom_dozons = self.product_uom_model.search([('name', '=', 'Dozen(s)')])[0]
 
         partner_id = self.res_partner_model.create(dict(name="George"))
+        partner_id_second = self.res_partner_model.create(dict(name="John"))
         tax_include_id = self.tax_model.create(dict(name="Include tax",
                                                     amount='21.00',
                                                     price_include=True,
@@ -38,6 +41,7 @@ class TestOnchangeProductId(TransactionCase):
         supplierinfo_vals = {
             'name': partner_id.id,
             'price': 121.0,
+            'min_qty': 5,
         }
 
         supplierinfo = self.supplierinfo_model.create(supplierinfo_vals)
@@ -71,3 +75,30 @@ class TestOnchangeProductId(TransactionCase):
         po_line = po.order_line[0]
         po_line.onchange_product_id()
         self.assertEquals(100, po_line.price_unit, "The included tax must be subtracted to the price")
+
+        po_line.write({'product_uom': uom_dozons.id})
+        po_line._onchange_quantity()
+        self.assertEquals(0, po_line.price_unit, "No vendor pricelist defined for %s with %s UoM!" % (partner_id.name, po_line.product_uom.name))
+
+        po_line.write({'product_qty': 4, 'product_uom': uom_id.id})
+        po_line._onchange_quantity()
+        self.assertEquals(0, po_line.price_unit, "No vendor pricelist defined for %s with %d quantities!" % (partner_id.name, po_line.product_qty))
+
+        po.write({
+            'partner_id': partner_id_second.id,
+            'order_line': [
+                (0, 0, {
+                        'product_id': self.product_id_2.id,
+                        'name': self.product_id_2.name,
+                        'product_qty': 5,
+                        'product_uom': uom_id.id,
+                        'price_unit': 100.0,
+                        'date_planned': datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT),
+                })],
+            })
+        po_line.onchange_product_id()
+        self.assertEquals(0, po_line.price_unit, "No vendor pricelist defined sp default price unit should be set to 0")
+
+        po_line.write({'price_unit': 100, 'product_qty': 5})
+        po_line._onchange_quantity()
+        self.assertEquals(500, po_line.price_subtotal, "Price subtotal should be %s" % po_line.price_subtotal)
