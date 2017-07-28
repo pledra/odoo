cf08aa1f5c6864b8063c1b23e00d106dc0c87c37
Jaykishan Solanki <jao@odoo.com>
2017-07-28 14:30:34 +0530

    [IMP] sale_order_dates: New computation of commitment / effective date on SO, Commitment Date:   - until the order is draft: commitment date = current date + lead time   - once the order confirmed: commitment date = confirmation date + lead time = expected delivery date (in picking)   Where lead time is,     - if Shipping Policy is 'Deliver All at once' then longest lead time from all order lines     - if Shipping Policy is 'Deliver once available' then shortest lead time from all order lines Effective Date:   - Date on which the first shipment successfully delivered to customer

--- addons/sale_order_dates/models/sale_order.py
+++ addons/sale_order_dates/models/sale_order.py
@@ -12,7 +12,7 @@ class SaleOrder(models.Model):
     commitment_date = fields.Datetime(compute='_compute_commitment_date', string='Commitment Date', store=True,
                                       help="Date by which the products are sure to be delivered. This is "
                                            "a date that you can promise to the customer, based on the "
-                                           "Product Lead Times.")
+                                           "Product Lead Times and shipping policy.")
     requested_date = fields.Datetime('Requested Date', readonly=True, states={'draft': [('readonly', False)],
                                      'sent': [('readonly', False)]}, copy=False,
                                      help="Date by which the customer has requested the items to be "
@@ -24,30 +24,28 @@ class SaleOrder(models.Model):
                                           "processed as soon as possible. In that case the expected "
                                           "date will be computed using the default method: based on "
                                           "the Product Lead Times and the Company's Security Delay.")
-    effective_date = fields.Date(compute='_compute_picking_ids', string='Effective Date', store=True,
-                                 help="Date on which the first Delivery Order was created.")
+    effective_date = fields.Date(compute='_compute_effective_date', string='Effective Date', store=True,
+                                 help="Date on which the first shipment is successfully delivered to customer.")
 
-    @api.depends('date_order', 'order_line.customer_lead')
+    @api.depends('order_line.customer_lead', 'confirmation_date', 'picking_policy', 'order_line.state')
     def _compute_commitment_date(self):
         """Compute the commitment date"""
         for order in self:
             dates_list = []
-            order_datetime = fields.Datetime.from_string(order.date_order)
+            confirm_date = fields.Datetime.from_string(order.confirmation_date or fields.Datetime.now())
             for line in order.order_line.filtered(lambda x: x.state != 'cancel'):
-                dt = order_datetime + timedelta(days=line.customer_lead or 0.0)
+                dt = confirm_date + timedelta(days=line.customer_lead or 0.0)
                 dates_list.append(dt)
             if dates_list:
                 commit_date = min(dates_list) if order.picking_policy == 'direct' else max(dates_list)
                 order.commitment_date = fields.Datetime.to_string(commit_date)
 
-    def _compute_picking_ids(self):
-        super(SaleOrder, self)._compute_picking_ids()
+    @api.depends('picking_ids.date_done')
+    def _compute_effective_date(self):
         for order in self:
-            dates_list = []
-            for pick in order.picking_ids:
-                dates_list.append(fields.Datetime.from_string(pick.date))
-            if dates_list:
-                order.effective_date = fields.Datetime.to_string(min(dates_list))
+            pickings = order.picking_ids.filtered(lambda x: x.state == 'done' and x.location_dest_id.usage == 'customer')
+            dates_list = pickings.mapped('date_done')
+            order.effective_date = dates_list and min(dates_list)
 
     @api.onchange('requested_date')
     def onchange_requested_date(self):
