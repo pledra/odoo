8048c3bcd3c15d72a9f7f72952d339d4e83bebc1
Olivier Colson <oco@odoo.com>
2017-10-26 10:05:39 +0200

    [IMP] l10n_es: add NIF field on companies

--- addons/l10n_es/__manifest__.py
+++ addons/l10n_es/__manifest__.py
@@ -45,5 +45,6 @@ Spanish charts of accounts (PGCE 2008).
         'data/account_tax_data.xml',
         'data/account_fiscal_position_template_data.xml',
         'data/account_chart_template_data.yml',
+        'views/res_company_views.xml',
     ],
 }
