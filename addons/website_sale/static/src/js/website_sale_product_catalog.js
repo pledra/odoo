326c47ebd861aed466befa9b7074d2eb8af09275
Kishan Gajjar <kig@odoo.com>
2017-10-30 18:47:29 +0530

    [ADD] website_sale: added widget that render product related stuff from backend and design template for catalog snippet

---
+++ addons/website_sale/static/src/js/website_sale_product_catalog.js
@@ -0,0 +1,88 @@
+odoo.define('website_sale.product_catalog', function (require) {
+'use strict';
+
+var ajax = require('web.ajax');
+var base = require('web_editor.base');
+var core = require('web.core');
+var rpc = require('web.rpc');
+var website = require('website.website');
+var Widget = require('web.Widget');
+var utils = require('web.utils')
+
+var QWeb = core.qweb;
+var ProductCatalog = Widget.extend({
+    template: 'website_sale.product_catalog',
+    xmlDependencies: ['/website_sale/static/src/xml/website_sale_product_catalog.xml'],
+    /**
+     * Initialize all options which are need to render widget.
+     * @constructor
+     * @override
+     * @param {jQuery} $target
+     */
+    init: function ($target) {
+        this._super.apply(this, arguments);
+        this.$target = $target;
+        this.catalogType = this.$target.attr('data-catalog-type');
+        this.size = 12/this.$target.attr('data-x');
+    },
+    /**
+     * Fetch product details.
+     *
+     * @override
+     * @returns {Deferred}
+     */
+    willStart: function () {
+        var self = this;
+        var def = rpc.query({
+            route: '/get_product_catalog_details',
+            params: {
+                'domain': this._getDomain(),
+                'sortby': this._getSortby(),
+                'limit': this._getLimit(),
+            }
+        }).then(function (result) {
+            self.products = result.products;
+        });
+        return $.when(this._super.apply(this, arguments), def);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+    /**
+     * formating description for the website sale display
+     *
+     * @private
+     * @param {string} get description.
+     * @returns {string} Contains string with replace '\n' to '<br>'.
+     */
+    _formatDescriptionValue: function (description_sale) {
+        return description_sale.split("\n").join("<br>");
+    },
+    _getDomain: function () {
+        var domain = [];
+        return domain;
+    },
+    _getSortby: function () {
+        var sortby = 'name ASC';
+        return sortby;
+    },
+    _getLimit: function () {
+        var limit;
+        limit = this.catalogType === 'grid' ? this.$target.attr('data-x') * this.$target.attr('data-y') :  this.$target.attr('data-carousel');
+        return limit;
+    },
+});
+base.ready().then(function () {
+    if ($('.s_product_catalog').length) {
+        $('.s_product_catalog').each(function () {
+            var productCatalog = new ProductCatalog($(this));
+            $(this).find('.product_grid').remove();
+            productCatalog.appendTo($(this).find('.container'));
+        });
+    }
+});
+return {
+    ProductCatalog: ProductCatalog
+};
+});
