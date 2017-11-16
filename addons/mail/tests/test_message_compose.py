02befe9e1bb3c1d6ab22853616d9584724afcfb1
Thibault Delavallée <tde@odoo.com>
2017-11-16 14:07:07 +0100

    mergeup: split test mail features -> test message compose + test discuss

--- addons/mail/tests/test_message_compose.py
+++ addons/mail/tests/test_message_compose.py
@@ -7,40 +7,6 @@ from odoo.addons.mail.tests.test_mail_gateway import MAIL_TEMPLATE_PLAINTEXT
 from odoo.tools import mute_logger
 
 
-class TestMailFeatures(TestMail):
-    # TDE TODO: tests on the redirection controller
-
-    def test_alias_setup(self):
-        alias = self.env['mail.alias'].with_context(alias_model_name='mail.test').create({'alias_name': 'b4r+_#_R3wl$$'})
-        self.assertEqual(alias.alias_name, 'b4r+_-_r3wl-', 'Disallowed chars should be replaced by hyphens')
-
-    def test_10_cache_invalidation(self):
-        """ Test that creating a mail-thread record does not invalidate the whole cache. """
-        # make a new record in cache
-        record = self.env['res.partner'].new({'name': 'Brave New Partner'})
-        self.assertTrue(record.name)
-
-        # creating a mail-thread record should not invalidate the whole cache
-        self.env['res.partner'].create({'name': 'Actual Partner'})
-        self.assertTrue(record.name)
-
-
-    @mute_logger('odoo.addons.mail.models.mail_mail')
-    def test_needaction(self):
-        # needaction use Inbox notification
-        (self.user_employee | self.user_admin).write({'notification_type': 'inbox'})
-
-        na_emp1_base = self.test_pigs.sudo(self.user_employee).message_needaction_counter
-        na_emp2_base = self.test_pigs.sudo().message_needaction_counter
-
-        self.test_pigs.message_post(body='Test', message_type='comment', subtype='mail.mt_comment', partner_ids=[self.user_employee.partner_id.id])
-
-        na_emp1_new = self.test_pigs.sudo(self.user_employee).message_needaction_counter
-        na_emp2_new = self.test_pigs.sudo().message_needaction_counter
-        self.assertEqual(na_emp1_new, na_emp1_base + 1)
-        self.assertEqual(na_emp2_new, na_emp2_base)
-
-
 class TestMessagePost(TestMail):
 
     @mute_logger('odoo.addons.mail.models.mail_mail')
