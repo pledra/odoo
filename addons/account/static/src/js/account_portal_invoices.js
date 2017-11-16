036f622fac612b4c1cda746c464b47d71e641740
kap-odoo <kap@odoo.com>
2017-11-16 19:03:57 +0530

    [ADD]account: changed design of account portal invoice.

---
+++ addons/account/static/src/js/account_portal_invoices.js
@@ -0,0 +1,36 @@
+odoo.define('account_portal_invoices.account_portal_invoices', function (require) {
+'use strict';
+
+require('web.dom_ready');
+var ajax = require('web.ajax');
+var config = require('web.config');
+var Widget = require('web.Widget');
+
+if(!$('.o_account_portal_invoices').length) {
+    return $.Deferred().reject("DOM doesn't contain '.o_account_portal_invoices'");
+}
+
+var $bs_sidebar = $(".o_account_portal_invoices .bs-sidebar");
+    $(window).on('resize', _.throttle(adapt_sidebar_position, 200, {leading: false}));
+    adapt_sidebar_position();
+
+    function adapt_sidebar_position() {
+        $bs_sidebar.css({
+            position: "",
+            width: "",
+        });
+        if (config.device.size_class >= config.device.SIZES.MD) {
+            $bs_sidebar.css({
+                position: "fixed",
+                width: $bs_sidebar.outerWidth(),
+            });
+        }
+    }
+
+    $bs_sidebar.affix({
+        offset: {
+            top: 0,
+            bottom: $('body').height() - $('#wrapwrap').outerHeight() + $("footer").outerHeight(),
+        },
+    });
+});
