082f608c561a0bb5994cf70d5f092434c3f80323
Olivier Colson <oco@odoo.com>
2017-11-16 14:34:17 +0100

    add NIF field on res.partner

--- addons/l10n_es/__manifest__.py
+++ addons/l10n_es/__manifest__.py
@@ -46,5 +46,6 @@ Spanish charts of accounts (PGCE 2008).
         'data/account_fiscal_position_template_data.xml',
         'data/account_chart_template_data.yml',
         'views/res_company_views.xml',
+        'views/res_partner_views.xml',
     ],
 }
