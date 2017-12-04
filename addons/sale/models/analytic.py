37b7dbb248e4b11dba45aec72c84b50bc93215ff
jem-odoo <jem@openerp.com>
2017-12-04 10:05:42 +0100

    test2

--- addons/sale/models/analytic.py
+++ addons/sale/models/analytic.py
@@ -141,6 +141,8 @@ class AccountAnalyticLine(models.Model):
                 so_line_values = analytic_line._sale_prepare_sale_order_line_values(sale_order, price)
                 so_line = self.env['sale.order.line'].create(so_line_values)
                 so_line._compute_tax_id()
+            else:
+                so_line.write({'qty_delivered': analytic_line.unit_amount})
 
             if so_line:  # if so line found or created, then update AAL (this will trigger the recomputation of qty delivered on SO line)
                 analytic_line.with_context(sale_analytic_norecompute=True).write({'so_line': so_line.id})
