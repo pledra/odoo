yf956613/odoo-1

[IMP] tests: Add the possibility to tag tests
Purpose: When running tests, all the tests for the installed/updated
files are done. This commit adds a 'set_tags' decorator that can be used to tag
tests. Combined with a new 'test-tags' CLI option, it adds the ability
to filter which tests are executed. For example, @set_tags('slow') will
add a tag 'slow' to the test. The CLI option 'test-tags="slow"' will
only run tests tagged 'slow'.

odoo/tests/common.py
@@ -472,3 +472,32 @@ def can_import(module):
         return False
     else:
         return True
+
+def set_tags(*tags):
+    """
+    A decorator to tag TestCase objects
+    Tags are stored in a set that can be accessed from a 'test_tags' attribute
+    """
+    def tags_decorator(obj):
+        if not hasattr(obj, 'test_tags'):
+            obj.test_tags = set()
+        obj.test_tags.update(set(tags))
+        return obj
+    return tags_decorator
+
+
+def has_tags(obj, tags):
+    """
+    Check if object 'obj' has test_tags 'tags' defined
+    Tags can be a set/list of tags or a comma seperated string
+    """
+    if not tags:
+        return True
+    if isinstance(tags, basestring):
+        tags = tags.split(',')
+    if not hasattr(obj, 'test_tags'):
+        _logger.debug('Test {} is not tagged'.format(obj))
+        return False
+    for t in tags:
+        if t in obj.test_tags:
+            return True

pledra/odoo

odoo/tests/common.py
@@ -477,3 +477,32 @@ def can_import(module):
         return False
     else:
         return True
+
+def set_tags(*tags):
+    """
+    A decorator to tag TestCase objects
+    Tags are stored in a set that can be accessed from a 'test_tags' attribute
+    """
+    def tags_decorator(obj):
+        if not hasattr(obj, 'test_tags'):
+            obj.test_tags = set()
+        obj.test_tags.update(set(tags))
+        return obj
+    return tags_decorator
+
+
+def has_tags(obj, tags):
+    """
+    Check if object 'obj' has test_tags 'tags' defined
+    Tags can be a set/list of tags or a comma seperated string
+    """
+    if not tags:
+        return True
+    if isinstance(tags, str):
+        tags = tags.split(',')
+    if not hasattr(obj, 'test_tags'):
+        _logger.debug('Test {} is not tagged'.format(obj))
+        return False
+    for t in tags:
+        if t in obj.test_tags:
+            return True