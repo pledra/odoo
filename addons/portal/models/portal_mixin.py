4c4290ee639ead729e987e2e940f457797901e3c
Thibault Delavallée <tde@odoo.com>
2017-12-04 17:06:59 +0100

    [IMP] portal: add access token link in emails

--- addons/portal/models/portal_mixin.py
+++ addons/portal/models/portal_mixin.py
@@ -3,7 +3,7 @@
 
 from werkzeug.urls import url_encode
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class PortalMixin(models.AbstractModel):
@@ -18,6 +18,20 @@ class PortalMixin(models.AbstractModel):
         for record in self:
             record.portal_url = '#'
 
+    def _get_access_token_field(self):
+        return 'access_token' if hasattr(self, 'access_token') else False
+
+    def _get_customer_field(self):
+        return 'partner_id' if hasattr(self, 'partner_id') else False
+
+    def _get_access_token(self):
+        field = self._get_access_token_field()
+        return getattr(self, field) if field else False
+
+    def _get_customer(self):
+        field = self._get_customer_field()
+        return getattr(self, field) if field else self.env['res.partner']
+
     def get_share_url(self):
         self.ensure_one()
         params = {
@@ -30,3 +44,28 @@ class PortalMixin(models.AbstractModel):
             params.update(self.partner_id.signup_get_auth_param()[self.partner_id.id])
 
         return '/mail/view?' + url_encode(params)
+
+    @api.multi
+    def _notification_recipients(self, message, groups):
+        access_token = self._get_access_token()
+        customer = self._get_customer()
+
+        if access_token and customer:
+            additional_params = {
+                'access_token': self.access_token,
+            }
+            additional_params.update(self.partner_id.signup_get_auth_param()[self.partner_id.id])
+            access_link = self._notification_link_helper('view', **additional_params)
+
+            new_group = [
+                ('portal_customer', lambda partner: partner.id == customer.id, {
+                    'has_button_access': True,
+                    'button_access': {
+                        'url': access_link,
+                        'title': _('Access'),
+                    },
+                })
+            ]
+        else:
+            new_group = []
+        return super(PortalMixin, self)._notification_recipients(message, new_group + groups)
