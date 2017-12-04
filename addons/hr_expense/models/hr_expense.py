60695e0f07cd619fed47cef4a87f0a3e3f7100fd
Nicolas Martinelli <nim@odoo.com>
2017-12-04 11:18:02 +0100

    [FIX] hr_expense: access error
    Commit 957d57eaf7b0 added the auto-subscription of managers to an
    expense at creation. However, this causes a conflict with the `ir.rule`
    `mail_followers_read_write_own`. If an employee has a manager, an access
    error is raised upon expense sheet creation.
    
    Adding a `sudo` is not an option, because the chatter will log the admin
    as the creator of the expense. Therefore, we are doing a workaround by
    using an intermediary 'Draft' state.
    
    opw-785416

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -386,12 +386,13 @@ class HrExpenseSheet(models.Model):
 
     name = fields.Char(string='Expense Report Summary', required=True)
     expense_line_ids = fields.One2many('hr.expense', 'sheet_id', string='Expense Lines', states={'approve': [('readonly', True)], 'done': [('readonly', True)], 'post': [('readonly', True)]}, copy=False)
-    state = fields.Selection([('submit', 'Submitted'),
+    state = fields.Selection([('draft', 'Draft'),
+                              ('submit', 'Submitted'),
                               ('approve', 'Approved'),
                               ('post', 'Posted'),
                               ('done', 'Paid'),
                               ('cancel', 'Refused')
-                              ], string='Status', index=True, readonly=True, track_visibility='onchange', copy=False, default='submit', required=True,
+                              ], string='Status', index=True, readonly=True, track_visibility='onchange', copy=False, default='draft', required=True,
         help='Expense Report State')
     employee_id = fields.Many2one('hr.employee', string="Employee", required=True, readonly=True, states={'submit': [('readonly', False)]}, default=lambda self: self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1))
     address_id = fields.Many2one('res.partner', string="Employee Home Address")
@@ -420,9 +421,11 @@ class HrExpenseSheet(models.Model):
 
     @api.model
     def create(self, vals):
-        self._create_set_followers(vals)
         sheet = super(HrExpenseSheet, self).create(vals)
         self.check_consistency()
+        if vals.get('employee_id'):
+            sheet._add_followers()
+        sheet.write({'state': 'submit'})
         return sheet
 
     @api.multi
@@ -457,34 +460,16 @@ class HrExpenseSheet(models.Model):
             return 'hr_expense.mt_expense_paid'
         return super(HrExpenseSheet, self)._track_subtype(init_values)
 
-    def _get_users_to_subscribe(self, employee=False):
-        users = self.env['res.users']
-        employee = employee or self.employee_id
-        if employee.user_id:
-            users |= employee.user_id
-        if employee.parent_id:
-            users |= employee.parent_id.user_id
-        if employee.department_id and employee.department_id.manager_id and employee.parent_id != employee.department_id.manager_id:
-            users |= employee.department_id.manager_id.user_id
-        return users
-
     def _add_followers(self):
-        users = self._get_users_to_subscribe()
-        self.message_subscribe_users(user_ids=users.ids)
-
-    @api.model
-    def _create_set_followers(self, values):
-        # Add the followers at creation, so they can be notified
-        employee_id = values.get('employee_id')
-        if not employee_id:
-            return
-
-        employee = self.env['hr.employee'].browse(employee_id)
-        users = self._get_users_to_subscribe(employee=employee) - self.env.user
-        values['message_follower_ids'] = []
-        MailFollowers = self.env['mail.followers']
-        for partner in users.mapped('partner_id'):
-            values['message_follower_ids'] += MailFollowers._add_follower_command(self._name, [], {partner.id: None}, {})[0]
+        user_ids = []
+        employee = self.employee_id
+        if employee.user_id:
+            user_ids.append(employee.user_id.id)
+        if employee.parent_id:
+            user_ids.append(employee.parent_id.user_id.id)
+        if employee.department_id and employee.department_id.manager_id and employee.parent_id != employee.department_id.manager_id:
+            user_ids.append(employee.department_id.manager_id.user_id.id)
+        self.message_subscribe_users(user_ids=user_ids)
 
     @api.onchange('employee_id')
     def _onchange_employee_id(self):
